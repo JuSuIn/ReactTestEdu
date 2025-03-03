4b720375907e70aa1b887d8ae6144cf9134adc17
SUINJU <jusuin5876@naver.com>
2025-03-03 16:39:01 +0900

    컴포넌트내에 단어를 동적으로 변경함(Dynamically change words in a component)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,30 +1,28 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {/* {reactDescriptions[genRandomInt(2)]}  */}
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
 
-Header();
-
 function App() {
   return (
     <div>
       <Header />
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
       <main>
         <h2>Time to get started!</h2>
       </main>
@@ -33,3 +31,39 @@ function App() {
 }
 
 export default App;
+
+// function Header() {
+//   return (
+//     <header>
+//       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+//       <h1>React Essentials</h1>
+//       <p>
+//         Fundamental React concepts you will need for almost any app you are
+//         going to build!
+//       </p>
+//     </header>
+//   );
+// }
+
+// Header();
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <header>
+//         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+//         <h1>React Essentials</h1>
+//         <p>
+//           Fundamental React concepts you will need for almost any app you are
+//           going to build!
+//         </p>
+//       </header>
+//       <main>
+//         <h2>Time to get started!</h2>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
