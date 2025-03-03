572a2ba2b266dbf1fe3992de5f12eba4921d6364
SUINJU <jusuin5876@naver.com>
2025-03-03 16:47:49 +0900

    React 동적으로 상대경로를 이용한 이미지 불러오기 (React dynamically load image using relative path)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -8,10 +10,10 @@ function Header() {
   const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      {/* "src/assets/react-core-concepts.png" */}
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {/* {reactDescriptions[genRandomInt(2)]}  */}
         {description} React concepts you will need for almost any app you are
         going to build!
       </p>
